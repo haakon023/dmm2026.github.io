778e4d0bfdeea37039599aecf595b8c3435262d8
Håkon Finstad <haakonfj@gmail.com>
2026-01-26 20:01:44 +0100

    update list

--- steps.js
+++ steps.js
@@ -1,124 +1,129 @@
 const checklistData = [
-      {
-          "title": "Start / Tutorial",
-          "items": [
-              { "label": "Logg inn" },
-              { "label": "Hans – check age", "points": "15 pts" },
-              { "label": "Pickpocket woman/man x3 for lvl 5 thieving (prøv å få bones)", "points": "15pts" },
-              { "label": "Hent anti-dragon shield" },
-              { "label": "Sjekk at du har dramenstaff eventuelt claim" },
-              { "label": "Sjekk at du har dramenstaff eventuelt claim", "note": "egen definert text" }
-          ]
-      },
-      {
-          "title": "Mage setup",
-          "items": [
-              { "label": "Velg mage item" },
-              { "label": "Defensive cast" },
-              { "label": "Cast alt du kan mens du løper" }
-          ]
-      },
-      {
-          "title": "Lumbridge & området",
-          "items": [
-              { "label": "Løp til HAM hideout og gå inn", "points": "15 pts" },
-              { "label": "Draynor Sewer – drep en zombie", "points": "15pts" },
-              { "label": "Draynor Agility Course – grind til minst 1 Mark of Grace", "points": "25pts" },
-              { "label": "Tele Lumbridge" }
-          ]
-      },
-      {
-          "title": "Stronghold",
-          "items": [
-              { "label": "Nigel" },
-              { "label": "Stronghold of Security – complete" },
-              { "label": "Gjør alle emotesa fra stronghold", "points": "15 pts" },
-              { "label": "Tele Varrock med Chronicle" }
-          ]
-      },
-      {
-          "title": "Varrock & Thieving",
-          "items": [
-              { "label": "Enter champions guild", "points": "25 pts" },
-              { "label": "Drep for bones (hvis du ikke har fått enda)" },
-              { "label": "Give dog a bone", "points": "15pts", "note": "HVIS DU FINN DOGGO" },
-              { "label": "Aubury kjøp water runes + mind runes" },
-              { "label": "Aubury teleport til Rune Essence Mine", "points": "15pts" },
-              { "label": "Steal tea fra varrock east" }
-          ]
-      },
-      {
-          "title": "Museum / Kudos",
-          "items": [
-              { "label": "Gå til Varrock Museum" },
-              { "label": "Nå 50 Kudos", "points": "15pts" }
-          ]
-      },
-      {
-          "title": "Planks",
-          "items": [
-              { "label": "Chop dying tree i Lumberyard", "points": "15 pts", "note": "DETTE KAN SKIPPES HVIS DÅRLIG TID ELLER GJØRES SENERE" },
-              { "label": "Sawmill – gjør logs til planks (trenger 100 gp)", "points": "15pts" }
-          ]
-      },
-      {
-          "title": "Fairy Ring & Mage Tower",
-          "items": [
-              { "label": "Tele Varrock" },
-              { "label": "Kjøp news paper", "points": "15pts" },
-              { "label": "Browse thalissa store", "points": "15pts", "note": "KJØPE SILK FFS" },
-              { "label": "Kjøp staff of air" },
-              { "label": "Gå GE -> spirit tree til wherever og tilbake igjen", "points": "25pts" },
-              { "label": "Løp til Edgeville, kjøpe team cape -> Så til fairy ring" },
-              { "label": "Fairy Ring: ALS", "points": "25pts" },
-              { "label": "Fairy Ring: AIR", "points": "25pts" },
-              { "label": "Fairy Ring: CIR", "points": "25pts" },
-              { "label": "Fairy Ring: DIS" },
-              { "label": "Mage Tower" },
-              { "label": "Teleport til Rune Essence Mine", "points": "15pts" },
-              { "label": "Sjekk om Lesser Demon er alive -> kill if not next step" },
-              { "label": "Tilbake til Fairy Ring" },
-              { "label": "Fairy Ring: BIS" }
-          ]
-      },
-      {
-          "title": "Ardougne",
-          "items": [
-              { "label": "Fairy Ring: BLR" },
-              { "label": "Ardougne Essence Mine", "points": "15pts" },
-              { "label": "Sell silk for 60 gp", "points": "15pts", "note": "SELG FOR 120, HAN TILBYR 50GP SELG FOR 60GP" },
-              { "label": "Steal cake", "points": "15pts" },
-              { "label": "Check pets @ Ardougne East Church", "points": "15pts" },
-              { "label": "Restore prayer @ Ardougne East Church", "points": "15pts" },
-              { "label": "Ardy lever to wilderness", "points": "30pts" }
-          ]
-      },
-      {
-          "title": "Prep for Lava Dragons",
-          "image": "wildyPathing.png",
-          "items": [
-              { "label": "Tele Varrock" },
-              { "label": "Husk runes + team cape" },
-              { "label": "Ikke la dragon bones gå i looting bag"}
-          ]
-      },
-      {
-          "title": "Lava Dragons",
-          "items": [
-              { "label": "Equip/reequip team cape ETTER du er ute i wildy", "points": "+15pts" },
-              { "label": "Drep en mammoth on the way", "points": "+15pts" },
-              { "label": "Drep en green drag mens du løper mot lava drags", "points": "+15pts" },
-              { "label": "Samle 5 lava dragon bones, ikke veldig farlig om vi ikke får 43, men minimum 39 for pray range + mage", "points": "+40pts" },
-              { "label": "Recharge Chaos altar", "points": "+15pts" },
-              { "label": "Chaos fnatic -> andre fnatic", "note": "treng range for this tho" },
-              { "label": "Zombie pirates for cash??" }
-          ]
-      },
-      {
-          "title": "Finishing up",
-          "items": [
-              { "label": "Museum quiz + lamps på prayer" },
-              { "label": "Museum quiz + lamps på prayer" }
-          ]
-      }
+    {
+        "title": "Start / Tutorial",
+        "items": [
+            { "label": "Logg inn" },
+            { "label": "Hans – check age", "points": "15 pts" },
+            { "label": "Pickpocket woman/man x3 for lvl 5 thieving", "note": "Prøv å få bones, kan få tak i senere i varrock", "points": "15pts" },
+            { "label": "Hent anti-dragon shield" },
+            { "label": "Sjekk at du har dramenstaff eventuelt claim" },
+        ]
+    },
+    {
+        "title": "Mage setup",
+        "items": [
+            { "label": "Velg mage item" },
+            { "label": "Defensive cast" },
+            { "label": "Cast alt du kan mens du løper" }
+        ]
+    },
+    {
+        "title": "Lumbridge & området",
+        "items": [
+            { "label": "Løp til HAM hideout og gå inn", "points": "15 pts" },
+            { "label": "Draynor Sewer – drep en zombie", "points": "15pts" },
+            { "label": "Draynor Agility Course – grind til minst 1 Mark of Grace", "points": "25pts" },
+            { "label": "Tele Lumbridge" }
+        ]
+    },
+    {
+        "title": "Stronghold",
+        "items": [
+            { "label": "Nigel" },
+            { "label": "Stronghold of Security – complete" },
+            { "label": "Gjør alle emotesa fra stronghold", "points": "15 pts" },
+            { "label": "Tele Varrock med Chronicle" }
+        ]
+    },
+    {
+        "title": "Varrock & Thieving",
+        "items": [
+            { "label": "Enter champions guild", "points": "25 pts" },
+            { "label": "Drep for bones (hvis du ikke har fått enda)" },
+            { "label": "Give dog a bone", "points": "15pts", "note": "HVIS DU FINN DOGGO" },
+            { "label": "Aubury kjøp water runes + mind runes" },
+            { "label": "Aubury teleport til Rune Essence Mine", "points": "15pts" },
+            { "label": "Steal tea fra varrock east", "points": "15pts" }
+        ]
+    },
+    {
+        "title": "Museum / Kudos",
+        "items": [
+            { "label": "Gå til Varrock Museum" },
+            { "label": "Nå 50 Kudos", "points": "15pts" }
+        ]
+    },
+    {
+        "title": "Planks",
+        "items": [
+            { "label": "Chop dying tree i Lumberyard", "points": "15 pts", "note": "DETTE KAN SKIPPES HVIS DÅRLIG TID ELLER GJØRES SENERE" },
+            { "label": "Sawmill – gjør logs til planks (trenger 100 gp)", "points": "15pts" }
+        ]
+    },
+    {
+        "title": "Fairy Ring & Mage Tower",
+        "items": [
+            { "label": "Tele Varrock" },
+            { "label": "Kjøp news paper", "points": "15pts" },
+            { "label": "Browse thalissa store", "points": "15pts", "note": "KJØPE SILK FFS" },
+            { "label": "Kjøp staff of air" },
+            { "label": "Gå GE -> spirit tree til wherever og tilbake igjen", "points": "25pts" },
+            { "label": "Løp til Edgeville, kjøpe team cape -> Så til fairy ring" },
+            { "label": "Fairy Ring: ALS", "points": "25pts" },
+            { "label": "Fairy Ring: AIR", "points": "25pts" },
+            { "label": "Fairy Ring: CIR", "points": "25pts" },
+            { "label": "Fairy Ring: DIS", "points": "25pts" },
+            { "label": "Mage Tower" },
+            { "label": "Teleport til Rune Essence Mine", "points": "15pts" },
+            { "label": "Sjekk om Lesser Demon er alive -> kill, if not next step", "points": "10pts" },
+            { "label": "Tilbake til Fairy Ring" },
+            { "label": "Fairy Ring: BIS", "points": "15pts" },
+            { "label": "Fairy Ring: BLR" }
+        ]
+    },
+    {
+        "title": "Ardougne",
+        "items": [
+            { "label": "Ardougne Essence Mine", "points": "15pts" },
+            { "label": "Sell silk for 60 gp", "points": "15pts", "note": "SELG FOR 120, HAN TILBYR 50GP SELG FOR 60GP" },
+            { "label": "Steal cake", "points": "15pts" },
+            { "label": "Check pets @ Ardougne East Church", "points": "15pts" },
+            { "label": "Restore prayer @ Ardougne East Church", "points": "15pts" },
+            { "label": "Ardy lever to wilderness", "points": "30pts" }
+        ]
+    },
+    {
+        "title": "Prep for Lava Dragons",
+        "image": "wildyPathing.png",
+        "items": [
+            { "label": "Tele Varrock" },
+            { "label": "Husk runes + team cape" },
+            { "label": "Ikke la dragon bones gå i looting bag" }
+        ]
+    },
+    {
+        "title": "Lava Dragons",
+        "items": [
+            { "label": "Equip/reequip team cape ETTER du er ute i wildy", "points": "+15pts" },
+            { "label": "Drep en mammoth on the way", "points": "+15pts" },
+            { "label": "Drep en green drag mens du løper mot lava drags", "points": "+15pts" },
+            { "label": "Samle 5 lava dragon bones, ikke veldig farlig om vi ikke får 43, men minimum 39 for pray range + mage", "points": "+40pts" },
+            { "label": "Recharge Chaos altar", "points": "+15pts", "note": "HUSK Å TRØKK PÅ ALTARN FOR Å RECHARGE!!!!!!!!!!!!!!!" },
+
+        ]
+    },
+    {
+        "title": "Finishing up",
+        "items": [
+            { "label": "Museum quiz + lamps på prayer" },
+            { "label": "Zombie pirates for cash?" },
+            { "label": "Chaos fnatic -> andre fnatic", "note": "treng range for this tho" },
+            { "label": "Crazed archeologist", "note": "Weak mot mage" },
+            { "label": "Scropia?", "note": "Treng anti poison? Dissa må gjøres i samme session hvis vi skal få diary task completion, Kan teames og weak til fire", "points": "40pts" },
+            { "label": "Varrock agility course", "points": "15pts" },
+            { "label": "Fence skip sør for varrock", "points": "15pts" },
+            { "label": "Scurrius", "points": "25pts" },
+
+        ]
+    }
 ];
